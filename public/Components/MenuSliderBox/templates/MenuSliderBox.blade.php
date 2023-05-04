36a030f320115c09636c16034346ea3294c287b1
Segy93 <41484839+Segy93@users.noreply.github.com>
2023-05-04 12:47:41 +0200

    izmene za meni

--- public/Components/MenuSliderBox/templates/MenuSliderBox.blade.php
+++ public/Components/MenuSliderBox/templates/MenuSliderBox.blade.php
@@ -1,8 +1,8 @@
-<div class = "menu_slider__box" >
+<section class = "menu_slider__box_menu">
+    {!! $main_menu->renderHTML() !!}
+</section>
+<div class = "menu_slider__box">
    <div class = "menu_slider__box_container">
-        <section class = "menu_slider__box_menu">
-            {!! $main_menu->renderHTML() !!}
-        </section>
 
         @if ($show_slider)
             <section
