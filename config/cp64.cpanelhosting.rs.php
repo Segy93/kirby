650778a364dacbd5fb5ab75aff478018fcda34b8
Segy93 <41484839+Segy93@users.noreply.github.com>
2023-06-23 12:58:30 +0200

    ispravka

---
+++ config/cp64.cpanelhosting.rs.php
@@ -0,0 +1,29 @@
+<?php
+
+use Monolog\Logger;
+
+return [
+    'AUTHENTICATE'              => true,
+    'DATABASE_TRUNCATE'         => true,
+    'DEVPASS'                   => true,
+    'EMAIL_SALES'               => 'segy993@gmail.com',
+    'FREE_SHIPPING_FEE_OVER'    => 15000,
+    'GOOGLE_ANALYTICS'          => false,
+    'GOOGLE_SECRET_KEY'         => '6LcZzGkUAAAAAFZg6OzLdmV9Elp1pJUtHSn9Kftn',
+    'GOOGLE_SITE_KEY'           => '6LcZzGkUAAAAAF3MY9bC9Kk0nOAPcOcFiJsTN_An',
+    'IMPORT_TOKEN'              => 'jqNpcYq2RExW8B3ouZZY21yArS0MbIow',
+    'LOG_LEVEL'                 => Logger::DEBUG,
+    'MINIFIER_DISABLE'          => true,
+    //
+    // Import
+    'PROGRESS_SHOW_HTML'        => true,
+    'PROGRESS_SLOW_DOWN'        => false,
+    'PUSH_PRIVATE'              => '7mptKG4OzY3YJzS8Fid9dtEPIPmPf7IYkuqo1Hg-oaY',
+    'PUSH_PUBLIC'               => 'BEGiJUS4kFRaFb-sJFm5o3ojSUEnJvo3nhfzO2DplUeFwYVQrRkol1XOSZxVc5B6fHd2qyLavP-KtNlDurn-iXA',
+    'RESPONSIVE_FILEMANAGER'    => '8FmK3b6QfuEQruSKBBDaSJJS',
+    'SEND_HEADERS'              => false,
+    'SHIPPING_FEE'              => 490,
+    'SKIP_EMAILS'               => true,
+    'TOKEN_ITSVET'              => 'Pn6KZXDQT4k2fSrR',
+    'VAPID_SUBJECT'             => 'mailto:segy993@gmail.com',
+];
