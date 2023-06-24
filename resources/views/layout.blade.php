c84a0c5bef95a1a6bfba41db5e0077d15c868b4d
Segy93 <41484839+Segy93@users.noreply.github.com>
2023-06-24 09:10:07 +0200

    ispravke

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html lang = "sr">
     <head >
-        <title>{{ !empty($seo->title) ? $seo->title : 'Kese za Kirby | eXelence d.o.o' }}</title>
+        <title>{{ !empty($seo->title) ? $seo->title : 'Kese za Kirby | eXelence d.o.o | Banovo brdo, Čukarica' }}</title>
 
         <base href="{{ (isset($_SERVER['HTTPS']) ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST']}}">
         <link href="apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72"/>
@@ -33,8 +33,8 @@
         <meta name="msapplication-TileImage" content="/mstile-144x144.png">
         <meta name="theme-color" content="#0056a9">
         <meta itemprop="name" content="{{ !empty($seo) ? $seo->title : 'Kese za Kirby' }}">
-        <meta name= "description" content="{{ !empty($seo->description) ? $seo->description : 'Kese za Kirby | eXelence d.o.o' }}">
-        <meta name= "keywords" content="{{ !empty($seo->keywords) ? $seo->keywords : 'Kese za Kirby, eXelence d.o.o, dodatna oprema, rezervni delovi' }}">
+        <meta name= "description" content="{{ !empty($seo->description) ? $seo->description : 'Kese za Kirby | eXelence d.o.o | Banovo brdo, Čukarica' }}">
+        <meta name= "keywords" content="{{ !empty($seo->keywords) ? $seo->keywords : 'Kese za Kirby, eXelence d.o.o, dodatna oprema, rezervni delovi, Banovo brdo, Čukarica' }}">
 
         @if (!empty($seo))
             @if (!empty($seo->image_open_graph))
@@ -74,8 +74,8 @@
             <meta property="og:url" content="{{ $base_url }}/{{ $seo->url !== 'pocetna' ? $seo->url : '' }}" />
             <meta property="og:site_name" content="Kirby" />
         @else
-            <meta name= "description" content="Kese za Kirby | eXelence d.o.o">
-            <meta name= "keywords" content="Kese za Kirby, eXelence d.o.o, dodatna oprema, rezervni delovi">
+            <meta name= "description" content="Kese za Kirby | eXelence d.o.o | Banovo brdo, Čukarica">
+            <meta name= "keywords" content="Kese za Kirby, eXelence d.o.o, dodatna oprema, rezervni delovi, Banovo brdo, Čukarica">
         @endif
         <meta name="format-detection" content="telephone=no">
 
