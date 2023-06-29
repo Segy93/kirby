875a85755254e569ab460d4cd3966e31a3746b9f
Segy93 <41484839+Segy93@users.noreply.github.com>
2023-06-29 12:11:38 +0200

    ispravka

--- app/Components/MainHeader.php
+++ app/Components/MainHeader.php
@@ -11,7 +11,7 @@ class MainHeader extends BaseComponent {
     protected $css  = ['MainHeader/css/MainHeader.css'];
     protected $js   = ['MainHeader/js/MainHeader.js'];
 
-    private $heading = 'Kirby servis <br/> kese, rezervni delovi, dodatna oprema i kompletni sistem <br/> Banovo Brdo, Čukarica, <a href = "tel:+38163223242">063/22-32-42</a>';
+    private $heading = 'Kirby servis <br/> <span class = "main_header__small">kese, rezervni delovi, dodatna oprema i kompletan aparat <br/> Beograd, Banovo Brdo, Čukarica, <a href = "tel:+38163223242">063/22-32-42</a></span>';
 
     public function __construct($heading = null) {
         if ($heading !== null) {
